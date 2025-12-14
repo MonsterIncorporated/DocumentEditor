57101a764f7bf87d63f92b8e0930dc8b1a4f7934
MonsterLuni <monsterluni@gmail.com>
2025-12-14 15:18:33 +0100

    add unit-testing possibility to backend

---
+++ Backend/document-editor-test/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using FluentAssertions;
+
+namespace document_editor_test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        var one = 1;
+        one.Should().Be(1);
+    }
+}
